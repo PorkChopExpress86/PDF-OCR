8ab6b90318c31f9749ec72817fcc869f7ab05274
Blake Bowden <blake.b.8726@gmail.com>
2020-10-09 17:07:11 -0500

    Setup will also clear out the input output folders

--- setup.py
+++ setup.py
@@ -1,5 +1,18 @@
 import os
-import platform
+import glob
+
+
+cwd = os.getcwd()
+
+clear_folders = input("Do you want to clear the following folders: pdf, jpg and output? Y/N ")
+
+if clear_folders.lower() == "y":
+    folder_paths = ["pdf", "jpg", "output"]
+    
+    for folders in folder_paths:
+        files = glob.glob(cwd + "/" + folders + "/*")
+        for f in files:
+            os.remove(f)
 
 try:
     os.mkdir("./pdf")
